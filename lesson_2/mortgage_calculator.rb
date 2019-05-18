50ca057dde0aded793274270ef4fb22a5a9abd98
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-18 12:36:41 +0900

    add mortgage calculator mesasges into yaml file

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,3 +1,15 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
+def prompt(key)
+  message = MESSAGES[key]
+  puts "=>#{message}"
+end
+
+def display_welcome
+  prompt('welcome')
+end
+
 def integer?(number)
   number.to_i.to_s == number
 end
@@ -7,70 +19,122 @@ def float?(number)
 end
 
 def valid_loan?(amount)
-  integer?(amount) && amount.to_f > 0
+  (integer?(amount) || float?(amount)) && amount.to_f > 0
 end
 
-def valid_apr?(apr)
-  integer?(apr) || float?(apr) && apr.to_f > 0
+def valid_interest_rate?(interest_rate)
+  (integer?(interest_rate) || float?(interest_rate)) && interest_rate.to_f > 0
 end
 
-def valid_years?(years)
+def valid_duration?(years)
   integer?(years) && years.to_i > 0
 end
 
-def prompt(message)
-  puts "=>#{message}"
-end
-
-prompt("Welcome to Mortgage calculator")
-
-loop do
+def retrieve_loan_amount
   amount = ''
   loop do
-    prompt("What is the total amount of loan in dollars? (i.e 10000)")
+    prompt('loan_amount')
     amount = gets.chomp
     if valid_loan?(amount)
       break
     else
-      prompt("Please enter a valid postive number.")
+      prompt('invalid_number')
     end
   end
-
-  annual_percentage_rate = ''
-  loop do
-    prompt("What is the Annual Percentage Rate? (i.e 7 or 7.0 for 7%)")
-    annual_percentage_rate = gets.chomp
-    if valid_apr?(annual_percentage_rate)
-      break
-    else
-      prompt("Please enter a valid postive number.")
-    end
-  end
-
-  years = ''
-  loop do
-    prompt("What is the loan duration in years? (i.e 30)")
-    years = gets.chomp
-    if valid_years?(years)
-      break
-    else
-      prompt("Please enter a valid positive number.")
-    end
-  end
-
-  monthly_interest_rate = annual_percentage_rate.to_f / 100 / 12
-  months = years.to_i * 12
-  monthly_payment_rate = amount.to_i * (monthly_interest_rate /
-                         (1 - (1 + monthly_interest_rate)**-months
-                         ))
-
-  # prompt("Your monthly payment is $#{monthly_payment_rate.round(2)}")
-  prompt("Your monthly payment is: $#{format('%02.2f', monthly_payment_rate)}")
-
-
-  prompt("Do you want to calculate again? (Y to go again)")
-  input = gets.chomp
-  break unless input.downcase == 'y' || input.downcase == 'yes'
+  amount
 end
 
-prompt("Thank you for using the Mortgage calculator. Goodbye")
+def retrieve_interest_rate
+  interest_rate = ''
+  loop do
+    prompt('interest_rate')
+    interest_rate = gets.chomp
+    if valid_interest_rate?(interest_rate)
+      break
+    else
+      prompt('invalid_number')
+    end
+  end
+  interest_rate
+end
+
+def retrieve_duration
+  years = ''
+  loop do
+    prompt('loan_duration')
+    years = gets.chomp
+    if valid_duration?(years)
+      break
+    else
+      prompt('invalid_number')
+    end
+  end
+  years
+end
+
+def calculate_annual_interest_rate(interest_rate)
+  interest_rate.to_f / 100
+end
+
+def calculate_monthly_interest_rate(annual_interest_rate)
+  annual_interest_rate.to_f / 12
+end
+
+def calculate_months(years)
+  years.to_i * 12
+end
+
+def calculate_monthly_payment_rate(amount, monthly_interest_rate, months)
+  amount.to_i * (monthly_interest_rate /
+                         (1 - (1 + monthly_interest_rate)**-months
+                         ))
+end
+
+def display_result(result)
+  puts("Your monthly payment is: $#{format('%02.2f', result)}")
+end
+
+def retrieve_calculate_again
+  answer = ''
+  prompt('calculate_again')
+
+  loop do
+    answer = gets.chomp
+    if %w(yes no y n).include?(answer.downcase)
+      break
+    else
+      prompt('choose_yes_or_no')
+    end
+  end
+  answer
+end
+
+def calculate_again?(answer)
+  answer.downcase == 'y' || answer.downcase == 'yes'
+end
+
+def display_goodbye
+  prompt('goodbye')
+end
+
+display_welcome
+
+loop do
+  amount = retrieve_loan_amount
+  interest_rate = retrieve_interest_rate
+  years = retrieve_duration
+
+  annual_interest_rate = calculate_annual_interest_rate(interest_rate)
+  monthly_interest_rate = calculate_monthly_interest_rate(annual_interest_rate)
+  months = calculate_months(years)
+  monthly_payment_rate = calculate_monthly_payment_rate(amount,
+                                                        monthly_interest_rate,
+                                                        months)
+
+  display_result(monthly_payment_rate)
+  answer = retrieve_calculate_again
+  break unless calculate_again?(answer)
+  system 'clear'
+end
+
+display_goodbye
