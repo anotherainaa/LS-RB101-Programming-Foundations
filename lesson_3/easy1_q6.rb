fb6b2856879810ac7cf4583e9f7ed6519241dfc1
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-06 19:32:31 +0900

    Add notes over question 6

--- lesson_3/easy1_q6.rb
+++ lesson_3/easy1_q6.rb
@@ -5,11 +5,24 @@ famous_words = "seven years ago..."
 # show two different ways to put the expected "Four score and " in front of it.
 
 # Solution 1 
+# Notes: This seems to modify the original object
 
 # famous_words.prepend("Four score and ")
 # p famous_words
 
 # Solution 2
+# Notes: This seems to modify the original object
 
 famous_words.insert(0, "Four score and ")
 p famous_words
+
+# LS Solutions
+
+# Notes: This solution does not modify the original object and must be saved in a new variable
+"Four score and " + famous_words
+
+# Notes: Same as my first solution.
+famous_words.prepend("Four score and ")
+
+# Notes: This solution does not modify the original object and must be saved in a new variable
+"Four score and " << famous_words
