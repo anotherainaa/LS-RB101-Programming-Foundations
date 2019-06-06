bf17ba1d81dadf0543cbb0fa7067c4034006ea69
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-06 19:22:56 +0900

    add solution to easy1 q6

---
+++ lesson_3/easy1_q6.rb
@@ -0,0 +1,15 @@
+# Question 6
+# Starting with the string:
+
+famous_words = "seven years ago..."
+# show two different ways to put the expected "Four score and " in front of it.
+
+# Solution 1 
+
+# famous_words.prepend("Four score and ")
+# p famous_words
+
+# Solution 2
+
+famous_words.insert(0, "Four score and ")
+p famous_words
