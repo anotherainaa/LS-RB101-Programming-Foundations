8d5163c66f3c1f2991cedbf7d00cdb97cd7b5e08
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-06 19:45:35 +0900

    add solution easy1 q9.rb

---
+++ lesson_3/easy1_q9.rb
@@ -0,0 +1,14 @@
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+# array = []
+# flintstones.each do |k, v|
+#   if k == "Barney"
+#     array << k
+#     array << v
+#   end
+# end
+# p array
+
+# LS solution
+
+flintstones.assoc("Barney")
