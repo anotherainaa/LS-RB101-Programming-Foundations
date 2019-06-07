887779cda692b135f00c8ff252df3469dc2de9ae
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-07 15:14:21 +0900

    add new solution medium1 q1

--- lesson_3/medium_1/q1.rb
+++ lesson_3/medium_1/q1.rb
@@ -5,12 +5,16 @@ string = 'The Flintstones Rock!'
 count = 1
 line_width = string.length
 
-until count >= 10 do
-  puts string.rjust(line_width)
-  count += 1
-  line_width += 1
-end
+# until count >= 10 do
+#   puts string.rjust(line_width)
+#   count += 1
+#   line_width += 1
+# end
 
 # LS Solution
 
-10.times {|number| puts (" " * number) + string }
+# 10.times {|number| puts (" " * number) + string }
+
+# Variation of my solution + LS solution
+
+10.times {|number| puts string.rjust(line_width + number)}
